5cbd795a76323d29f8c7ba703485d863155ba498
Stelmakh Bohdan <bodya300595@gmail.com>
2021-03-25 22:26:58 +0200

    was added footer to project

---
+++ src/shared/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import { Container, Wrapper } from './StyledComponents';
+
+const Footer = () => {
+  return (
+    <Wrapper>
+      <Container>Cinema world</Container>
+    </Wrapper>
+  );
+};
+
+export default Footer;
