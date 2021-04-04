d2aeca97578cecbdcc01d4808380fbd715f924d3
Stelmakh Bohdan <bodya300595@gmail.com>
2021-04-04 11:11:53 +0300

    was added Footer and Header on Main page

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -1,14 +1,19 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
+import Header from '../../shared/components/Header';
+import Footer from '../../shared/components/Footer';
 import { Wrapper, Title } from './StyledComponents';
 
 const Home: FC = () => {
   return (
     <Wrapper>
+      <Header />
       <Title>Home Page</Title>
 
       <Link to="/dashboard">Going to Dashboard</Link>
+
+      <Footer />
     </Wrapper>
   );
 };
--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { Container, Wrapper } from './StyledComponents';
 const Footer = () => {
   return (
     <Wrapper>
-      <Container>Cinema world</Container>
+      <Container>best programmers</Container>
     </Wrapper>
   );
 };
