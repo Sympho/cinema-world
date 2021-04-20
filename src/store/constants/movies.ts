dc81a2625f1a8cdc1fdaee9d46efc4b0beb4c3c1
Stelmakh Bohdan <bodya300595@gmail.com>
2021-04-20 20:00:49 +0300

    was changed store type for home page

--- src/store/constants/movies.ts
+++ src/store/constants/movies.ts
@@ -1,37 +1,55 @@
 const movies = [
   {
+    id: 1,
     title: 'Avengers',
     isComplete: true,
+    imageBg:
+      'https://i1.wp.com/itc.ua/wp-content/uploads/2020/02/big_startfilmru1436207-1.jpg?fit=1280%2C800&quality=100&strip=all&ssl=1',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
   {
+    id: 2,
     title: 'Avengers',
     isComplete: false,
+    imageBg:
+      'https://multiplex.ua/images/3e/57/3e571d60cc4bfae24e1b29b272328fd9.jpeg',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
   {
+    id: 3,
     title: 'Avengers',
     isComplete: false,
+    imageBg:
+      'https://multiplex.ua/images/3e/57/3e571d60cc4bfae24e1b29b272328fd9.jpeg',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
   {
+    id: 4,
     title: 'Avengers',
     isComplete: false,
+    imageBg:
+      'https://multiplex.ua/images/3e/57/3e571d60cc4bfae24e1b29b272328fd9.jpeg',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
   {
+    id: 5,
     title: 'Avengers',
     isComplete: false,
+    imageBg:
+      'https://multiplex.ua/images/3e/57/3e571d60cc4bfae24e1b29b272328fd9.jpeg',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
   {
+    id: 6,
     title: 'Avengers',
     isComplete: false,
+    imageBg:
+      'https://multiplex.ua/images/3e/57/3e571d60cc4bfae24e1b29b272328fd9.jpeg',
     link:
       'https://rezka.ag/films/drama/38175-bratya-po-krovi-grazhdanskaya-voyna-2021.html',
   },
