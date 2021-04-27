3991b170aad4c3c853b4a5b9890f07f980fbed2b
Stelmakh Bohdan <bodya300595@gmail.com>
2021-04-27 12:58:30 +0300

    added extra text

--- src/views/Dashboard/Dashboard.tsx
+++ src/views/Dashboard/Dashboard.tsx
@@ -8,10 +8,18 @@ const DashBoard: FC<DashBoardProps> = ({ text }) => {
     <Container>
       <Main>
         <h1>Dashboard</h1>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
-        blanditiis doloremque ea eos error ex ,impedit iste labore magnam minus
-        nostrum officia, omnis quasi quibusdam reiciendis rerum sint ut
-        voluptatum.
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+          blanditiis doloremque ea eos error ex ,impedit iste labore magnam
+          minus nostrum officia, omnis quasi quibusdam reiciendis rerum sint ut
+          voluptatum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+          blanditiis doloremque ea eos error ex ,impedit iste labore magnam
+          minus minus nostrum officia, omnis quasi quibusdam reiciendis rerum
+          sint ut voluptatum.
+        </p>
       </Main>
       {text}
     </Container>
