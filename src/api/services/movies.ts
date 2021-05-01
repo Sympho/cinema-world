3b4e04f725568e0303f45a8d1d83881bf5e36a35
Azim <mieertha@ukr.net>
2021-05-01 23:43:56 +0300

    added method getItem

--- src/api/services/movies.ts
+++ src/api/services/movies.ts
@@ -4,3 +4,8 @@ export const getItems = async () => {
   const res = await httpClient.get('/movies');
   return res.data;
 };
+
+export const getItem = async (id: number) => {
+  const res = await httpClient.get(`/movies/${id}`);
+  return res.data;
+};
