9a2cd1d418a1f77963bb8cded9edc93451be417c
Stelmakh Bohdan <bodya300595@gmail.com>
2021-05-01 18:04:44 +0300

    was created movieStats

--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -18,3 +18,22 @@ export const filteredTodoListState = selector({
     }
   },
 });
+
+export const movieStatsState = selector({
+  key: 'movieStatsState',
+  get: ({ get }) => {
+    const movieList = get(movieState);
+    const totalNum = movieList.length;
+    const totalWatchedNum = movieList.filter(movie => movie.isComplete).length;
+    const totalUnwatchedNum = totalNum - totalWatchedNum;
+    const percentWatched =
+      totalNum === 0 ? 0 : (totalWatchedNum / totalNum) * 100;
+
+    return {
+      totalNum,
+      totalWatchedNum,
+      totalUnwatchedNum,
+      percentWatched,
+    };
+  },
+});
