e22ba4b0f1f275877c7b135c48d5af275d99d9d5
Stelmakh Bohdan <bodya300595@gmail.com>
2021-05-09 19:21:16 +0300

    was created Schedule component

---
+++ src/views/Schedule/Schedule.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import Page from 'shared/components/Page';
+import { ScheduleProps } from './types';
+
+const Schedule: FC<ScheduleProps> = () => {
+  return (
+    <Page>
+      <h1>Schedule</h1>
+    </Page>
+  );
+};
+
+export default Schedule;
