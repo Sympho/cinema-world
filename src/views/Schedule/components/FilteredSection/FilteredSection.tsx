c2a43af4ef1b0203d4dd3da339b6a5ef66f29c99
Stelmakh Bohdan <bodya300595@gmail.com>
2021-05-10 11:33:26 +0300

    was created filtered section and galleryItem

---
+++ src/views/Schedule/components/FilteredSection/FilteredSection.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+import { Wrapper } from './styled';
+import { FilteredSectionProps } from './types';
+
+const FilteredSection: FC<FilteredSectionProps> = () => {
+  return (
+    <Wrapper>
+      <span>sdasdasdasd</span>
+    </Wrapper>
+  );
+};
+
+export default FilteredSection;
---
+++ src/views/Schedule/components/Gallery/GalleryItem/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 5px;
+`;
