ba67d68a97d703ab3979e96df9934100d4393c60
Stelmakh Bohdan <bodya300595@gmail.com>
2021-05-14 09:37:12 +0300

    was increased gallery item

--- src/views/Schedule/components/Gallery/GalleryItem/GalleryItem.tsx
+++ src/views/Schedule/components/Gallery/GalleryItem/GalleryItem.tsx
@@ -3,8 +3,30 @@ import { FC } from 'react';
 import { Wrapper } from './styled';
 import { GalleryItemProps } from './types';
 
-const GalleryItem: FC<GalleryItemProps> = () => {
-  return <Wrapper></Wrapper>;
+const GalleryItem: FC<GalleryItemProps> = ({
+  title,
+  day,
+  img,
+  visibleTime,
+  technology,
+}) => {
+  return (
+    <Wrapper>
+      <div>{img}</div>
+      <div>
+        <span>{title}</span>
+        <span>{day}</span>
+        <div>
+          {technology.map(technologyItem => (
+            <span>{technologyItem}</span>
+          ))}
+          {visibleTime.map(time => (
+            <span>{time}</span>
+          ))}
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default GalleryItem;
