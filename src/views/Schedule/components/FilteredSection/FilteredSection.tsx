d6c4bbd7d2873ee4b8abc09d15bc99fda0c5bc21
Stelmakh Bohdan <bodya300595@gmail.com>
2021-05-21 09:51:12 +0300

    was added periodData to filtered section

--- src/views/Schedule/components/FilteredSection/FilteredSection.tsx
+++ src/views/Schedule/components/FilteredSection/FilteredSection.tsx
@@ -1,13 +1,33 @@
 import { FC } from 'react';
 
-import { Wrapper } from './styled';
+import Checkbox from 'shared/components/CheckBox';
+import { Wrapper, Title } from './styled';
 import { FilteredSectionProps } from './types';
+
+const periodData = [
+  { title: 'today', value: 'today' },
+  { title: 'tomorrow', value: 'tomorrow' },
+  { title: 'week', value: 'week' },
+  { title: 'month', value: 'month' },
+];
 
 const FilteredSection: FC<FilteredSectionProps> = () => {
   return (
     <Wrapper>
-      <span>Filter Section</span>
-      <span>Period</span>
+      <Title>
+        <span>Filter Icon </span>
+        Filter Section
+      </Title>
+      <div>
+        <title>Period</title>
+        {periodData.map(period => (
+          <div key={period.title}>
+            <Checkbox checked={!!period.value} handleChange={() => {}} />
+            <span>{period.title}</span>
+          </div>
+        ))}
+      </div>
+
       <span>Technology</span>
       <span>Format</span>
       <span>Another</span>
--- src/views/Schedule/components/FilteredSection/styled.ts
+++ src/views/Schedule/components/FilteredSection/styled.ts
@@ -6,3 +6,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
 `;
+
+export const Title = styled.span`
+  font-size: 14px;
+`;
